b5a03cb7c87d23ed73dcf514f4851f2a7e90b3e2
David Hardy <davehardy632@gmail.com>
2019-09-05 11:41:58 -0600

    Made api call and returning the word.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,40 @@
 import $ from 'jquery'
 
+function createNode(element) {
+  return document.createElement(element);
+}
+
+function append(parent, el) {
+  return parent.appendChild(el);
+}
+
+const ul = document.querySelector("#word-list");
+
+
+const wordCallUrl = "https://wordwatch-api.herokuapp.com/api/v1/top_word"
+
 $(document).ready(() => {
-  // have fun!
+  renderWordAndCount(wordCallUrl);
 })
+
+function renderWordAndCount(url) {
+  fetch(url)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(wordObj) {
+    // console.log(Object.keys(wordObj.word)[0])
+    // var li = document.createElement('li')
+    //
+    // let word_key = Object.keys(wordObj.word)[0]
+    // li.innerHTML = word_key
+    const header = document.querySelector("#head");
+    // console.log(header.innerHTML + Object.keys(wordObj.word)[0])
+
+    header.innerHTML = header.innerHTML + Object.keys(wordObj.word)[0]
+
+  })
+  .catch(error => {
+    return error
+  });
+}
